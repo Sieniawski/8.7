b895a69dc53cfc2015679717b0bf535a74ae7acb
Wojciech <sieniawski7@wp.pl>
2017-02-17 20:09:07 +0100

    Update 8.7script.js

--- js/8.7script.js
+++ js/8.7script.js
@@ -1,10 +1,3 @@
-var test = [1, 2, 3];
-var test2 = ['a', 'b', 'c'];
-var test3 = ['x', 'y', 'z'];
-var arr = test.concat(test2,test3);
-
-console.log(arr);
-
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
 var allNames = femaleNames.concat(maleNames);

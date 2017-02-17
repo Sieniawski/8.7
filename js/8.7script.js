c01858057396523b52353c2a7216aadef2ab31cf
sieniawski <sieniawski7@wp.pl>
2017-02-17 20:07:54 +0100

    it finnaly works

--- js/8.7script.js
+++ js/8.7script.js
@@ -1,13 +1,19 @@
+var test = [1, 2, 3];
+var test2 = ['a', 'b', 'c'];
+var test3 = ['x', 'y', 'z'];
+var arr = test.concat(test2,test3);
+
+console.log(arr);
+
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
 var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-var allNames = femaleNames.concat(maleNames)
+var allNames = femaleNames.concat(maleNames);
 
-alert(allNames);
+console.log(allNames)
 
 var newName ='Marian'
 
 if (allNames.indexOf(newName) === -1) {
-	allNames.push(newName)
-} 
-
-// Trzeba dokończyć!
+	allNames.push(newName);
+	console.log(allNames);
+}
